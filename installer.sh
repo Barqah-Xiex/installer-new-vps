036ba0d365709caf05c7f067eabb9dfb485e0b66
Barqah <71264201+Barqah-Xiex@users.noreply.github.com>
2022-11-27 08:03:15 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,24 @@
+echo "Memperbarui apt..."
 apt upgrade -y;
 apt update -y;
+echo "menginstall package..."
 apt install curl ffmpeg wget zip unzip -y;
+echo "SetUp Nodejs..."
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -;
 sleep 3;
 apt update;
 apt install nodejs -y;
 npm install pm2 -g;
+echo "SetUp canvas..."
 sudo apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev -y;
+echo "SetUp SSH..."
 cp -rf template /etc/ssh/sshd_config;
 systemctl restart ssh;
+echo "Mengubah TimeZone..."
+sudo timedatectl set-timezone Asia/Jakarta;
+echo "Masukan Link Bot: ";
+read link
+wget $link;
+rm -rf *.zip;
+unzip *.zip;
+echo selesai
